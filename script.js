bd37f4f9ebcf7bf251d8086294cc45e3501e326b
Sreerag Rajan <98447986+SreeragRajan@users.noreply.github.com>
2024-03-10 13:25:03 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,197 +1,198 @@
-
-// Shery.mouseFollower();
-
-
-var allH1 = document.querySelectorAll("#description h1")
-
-allH1.forEach(function(elem) {
-  var clutter = ""
-  var h1Text = elem.textContent
-  var splittedText = h1Text.split("")
-  splittedText.forEach(function(e){
-    clutter += `<span>${e}</span>`
-  })
-  elem.innerHTML = clutter
-  // console.log(elem)
-})
-
-
-
-
-function init(){
-  gsap.registerPlugin(ScrollTrigger);
-
-// Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
-
-const locoScroll = new LocomotiveScroll({
-  el: document.querySelector(".container"),
-  smooth: true
-});
-// each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
-locoScroll.on("scroll", ScrollTrigger.update);
-
-// tell ScrollTrigger to use these proxy methods for the ".container" element since Locomotive Scroll is hijacking things
-ScrollTrigger.scrollerProxy(".container", {
-  scrollTop(value) {
-    return arguments.length ? locoScroll.scrollTo(value, 0, 0) : locoScroll.scroll.instance.scroll.y;
-  }, // we don't have to define a scrollLeft because we're only scrolling vertically.
-  getBoundingClientRect() {
-    return {top: 0, left: 0, width: window.innerWidth, height: window.innerHeight};
-  },
-  // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
-  pinType: document.querySelector(".container").style.transform ? "transform" : "fixed"
-});
-
-// each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll. 
-ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
-
-// after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
-ScrollTrigger.refresh();
-
-}
-
-init()
-
-
-// cursor
 var cursor = document.getElementById("cursor");
 var resumebtn = document.querySelector("#resume");
 var progress = document.querySelector(".progressbar");
+var allH1 = document.querySelectorAll("#description h1");
+var links = document.querySelectorAll("#footer a");
 
-function progressAnimation () {
-  var setwidth = 0;
-  setInterval(function() {
-    if(setwidth < 100) {
-      setwidth++;
-       progress.style.width = `${setwidth}%`;
-    } 
-    else {
-      progress.style.width = `${setwidth}%`;
-    }
-  }, 30)
-
+function textEffect() {
+  allH1.forEach(function (elem) {
+    var clutter = "";
+    var h1Text = elem.textContent;
+    var splittedText = h1Text.split("");
+    splittedText.forEach(function (e) {
+      clutter += `<span>${e}</span>`;
+    });
+    elem.innerHTML = clutter;
+  });
 }
 
-progressAnimation();
+function init() {
+  gsap.registerPlugin(ScrollTrigger);
 
-document.addEventListener("mouseenter", (e) => {
-  gsap.to(cursor, {
-    scale: 1,
-  })
-})
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
 
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector(".container"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
 
-document.addEventListener("mousemove", (e) => {
-  gsap.to(cursor, {
-    top: e.clientY + "px",
-    left: e.clientX + "px",
-  })
-})
+  // tell ScrollTrigger to use these proxy methods for the ".container" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy(".container", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector(".container").style.transform
+      ? "transform"
+      : "fixed",
+  });
 
-document.addEventListener("mouseleave", (e) => {
-  gsap.to(cursor, {
-    scale: 0,
-  })
-})
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
 
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+}
 
+function progressAnimation() {
+  var setwidth = 0;
+  setInterval(function () {
+    if (setwidth < 100) {
+      setwidth++;
+      progress.style.width = `${setwidth}%`;
+    } else {
+      progress.style.width = `${setwidth}%`;
+    }
+  }, 30);
+}
 
-resumebtn.addEventListener("mousemove", (e) => {
-  cursor.innerHTML = "<h4>Open</h4>"
-  gsap.to(cursor, {
-    height: "90px",
-    width: "90px",
-    backgroundColor: "black",
-    mixBlendMode: "normal",
-    
-  })
-})
+function customCursor() {
+  document.addEventListener("mouseenter", (e) => {
+    gsap.to(cursor, {
+      scale: 1,
+    });
+  });
 
-resumebtn.addEventListener("mouseleave", (e) => {
-  cursor.innerHTML = ""
-  gsap.to(cursor, {
-    height: "15px",
-    width: "15px",
-    backgroundColor: "white",
-    mixBlendMode: "difference",
-  })
-})
+  document.addEventListener("mousemove", (e) => {
+    gsap.to(cursor, {
+      top: e.clientY + "px",
+      left: e.clientX + "px",
+    });
+  });
 
+  document.addEventListener("mouseleave", (e) => {
+    gsap.to(cursor, {
+      scale: 0,
+    });
+  });
+}
 
-gsap.to("#home .home-content .name", {
-  x:-300,
-  scrollTrigger: {
-    trigger:"#home .name",
-    scroller:".container",
-    start:"top 50%",
-    end:"top 0%",
-    scrub:2
-  }
-})
+function resumebtnEffect() {
+  resumebtn.addEventListener("mousemove", (e) => {
+    cursor.innerHTML = "<h4>Open</h4>";
+    gsap.to(cursor, {
+      height: "90px",
+      width: "90px",
+      backgroundColor: "black",
+      mixBlendMode: "normal",
+    });
+  });
 
-gsap.to("#home .home-content .developer", {
-  x:300,
-  scrollTrigger: {
-    trigger:"#home .developer",
-    scroller:".container",
-    start:"top 60%",
-    end:"top 10%",
-    scrub:2
-  }
-})
+  resumebtn.addEventListener("mouseleave", (e) => {
+    cursor.innerHTML = "";
+    gsap.to(cursor, {
+      height: "15px",
+      width: "15px",
+      backgroundColor: "white",
+      mixBlendMode: "difference",
+    });
+  });
+}
 
-gsap.to(".scroll .arrow", {
-  y:-15,
-  repeat:-1,
-  yoyo:1,
-})
+function homeAnimation() {
+  gsap.to("#home .home-content .name", {
+    x: -300,
+    scrollTrigger: {
+      trigger: "#home .name",
+      scroller: ".container",
+      start: "top 50%",
+      end: "top 0%",
+      scrub: 2,
+    },
+  });
 
-gsap.from(".container #home #nav", {
-  y:-100,
-  duration:1,
-  opacity:0,
-  delay: 4,
+  gsap.to("#home .home-content .developer", {
+    x: 300,
+    scrollTrigger: {
+      trigger: "#home .developer",
+      scroller: ".container",
+      start: "top 60%",
+      end: "top 10%",
+      scrub: 2,
+    },
+  });
 
-})
+  gsap.from(
+    "#home .home-content .name, #home .home-content .developer, #home .home-content .greeting",
+    {
+      y: 100,
+      opacity: 0,
+      delay: 4,
+      ease: "power4.inOut",
+    }
+  );
 
+  gsap.to(".scroll .arrow", {
+    y: -15,
+    repeat: -1,
+    yoyo: 1,
+  });
+}
 
-gsap.from("#home .home-content .name, #home .home-content .developer, #home .home-content .greeting", {
-  y:100,
-  opacity:0,
-  delay: 4,
-  ease: "power4.inOut"
-})
-
-gsap.from(".loading h1", {
-    opacity:0,
+function loadingAnimation() {
+  gsap.from(".loading h1", {
+    opacity: 0,
     duration: 2,
-    ease: "power4.inOut"
-})
+    ease: "power4.inOut",
+  });
 
-
-gsap.to(".loading", {
+  gsap.to(".loading", {
     top: "-100%",
-    delay:3.3,
+    delay: 3.3,
     duration: 1,
-    ease: "power4.inOut"
-})
+    ease: "power4.inOut",
+  });
+}
 
+function navAnimation() {
+  gsap.from(".container #home #nav", {
+    y: -100,
+    duration: 1,
+    opacity: 0,
+    delay: 4,
+  });
+}
 
-gsap.to("#description h1 span", {
-  color: "white",
-  stagger: 0.1,
-  scrollTrigger: {
-    trigger: "#description h1",
-    scroller: ".container",
-    start: "top 50%",
-    end: "top 15%",
-    scrub: 3,
-  }
-})
+function descAnimation() {
+  gsap.to("#description h1 span", {
+    color: "white",
+    stagger: 0.1,
+    scrollTrigger: {
+      trigger: "#description h1",
+      scroller: ".container",
+      // markers: true,
+      start: "top 50%",
+      end: "top 15%",
+      scrub: 3,
+    },
+  });
+}
 
-if(window.innerWidth > 500) {
+function imgAnimation() {
   gsap.to("#about img", {
-    transform: "translatey(-20%)",
+    transform: "translateY(-20%)",
     scrollTrigger: {
       trigger: "#about",
       scroller: ".container",
@@ -199,96 +200,121 @@ if(window.innerWidth > 500) {
       start: "25% 10%",
       end: "20% -80%",
       pin: true,
-  
-    }
-  })
-
+    },
+  });
 }
 
+function footerAnimation() {
+  links.forEach((link) => {
+    link.addEventListener("mousemove", (e) => {
+      cursor.innerHTML = "<h4>Open</h4>";
+      gsap.to(cursor, {
+        height: "10vw",
+        width: "10vw",
+        color: "black",
+        backgroundColor: "white",
+        fontSize: "1.5vw",
+        mixBlendMode: "normal",
+      });
+    });
 
-var links = document.querySelectorAll("#footer a")
+    link.addEventListener("mouseleave", (e) => {
+      cursor.innerHTML = "";
+      gsap.to(cursor, {
+        height: "15px",
+        width: "15px",
+        fontSize: "14px",
+        color: "white",
+        backgroundColor: "white",
+        mixBlendMode: "difference",
+      });
+    });
+  });
+}
 
-links.forEach((link) => {
-  link.addEventListener("mousemove", (e) => {
-    cursor.innerHTML = "<h4>Open</h4>"
-    gsap.to(cursor, {
-      height: "10vw",
-      width: "10vw",
-      color: "black",
-      backgroundColor: "white",
-      fontSize: "1.5vw",
-      mixBlendMode: "normal",
-      
-    })
-  })
-  
-  link.addEventListener("mouseleave", (e) => {
-    cursor.innerHTML = ""
-    gsap.to(cursor, {
-      height: "15px",
-      width: "15px",
-      fontSize: "14px",
-      color: "white",
-      backgroundColor: "white",
-      mixBlendMode: "difference",
-    })
-  })
-  
-})
+function skillAnimation() {
+  gsap.from("#skills #skill-set #skill-card", {
+    x: -100,
+    opacity: 0,
+    stagger: 0.2,
+    duration: 1.5,
+    ease: "Power4InOut",
+    scrollTrigger: {
+      trigger: "#skill-card",
+      scroller: ".container",
+      // markers: true,
+      scrub: 2,
+      start: "-110% 50%",
+      end: "top 60%",
+    },
+  });
+}
 
-gsap.from("#skills #skill-set #skill-card", {
-  x: -100,
-  opacity: 0,
-  stagger: 0.2,
-  duration: 1.5,
-  ease: "Power4InOut",
-  scrollTrigger: {
-    trigger: "#skill-card",
-    scroller: ".container",
-    // markers: true,
-    scrub: 2,
-    start: "-110% 50%",
-    end: "top 60%",
-  }
-})
-
-var tl = gsap.timeline({
-  scrollTrigger: {
-    trigger: "#description",
-    scroller: ".container",
-    start: "top 30%",
-    end: "top 70%",
-    scrub: 2,
-    duration: 1,
-  }
-})
-
-tl.to(".container", {
-  backgroundColor: "black",
-})
-
-
-var tl2 = gsap.timeline({
-  scrollTrigger: {
-    trigger: "#about",
-    scroller: ".container",
-    start: "top 30%",
-    end: "top 70%",
-    scrub: 2,
-    duration: 1,
-  }
-})
-
-tl2.to(".container", {
-  backgroundColor: "white",
-})
-
-
-function sheryAnimation () {
+function sheryAnimation() {
   Shery.imageEffect(".img-box", {
     style: 5,
     gooey: true,
-  })
+  });
 }
 
+function backgroundColorAnimation() {
+  var tl = gsap.timeline({
+    scrollTrigger: {
+      // markers: true,
+      trigger: "#description",
+      scroller: ".container",
+      start: "top 30%",
+      end: "top 70%",
+      scrub: 2,
+      duration: 1,
+    },
+  });
+
+  tl.to(".container", {
+    backgroundColor: "black",
+  });
+
+  var tl2 = gsap.timeline({
+    scrollTrigger: {
+      // markers: true,
+      trigger: "#about",
+      scroller: ".container",
+      start: "top 30%",
+      end: "top 70%",
+      scrub: 2,
+      duration: 1,
+    },
+  });
+
+  tl2.to(".container", {
+    backgroundColor: "white",
+  });
+}
+
+init();
+customCursor();
+loadingAnimation();
+progressAnimation();
+navAnimation();
+resumebtnEffect();
+homeAnimation();
+backgroundColorAnimation();
+textEffect();
+descAnimation();
+imgAnimation();
+skillAnimation();
 sheryAnimation();
+footerAnimation();
+
+// gsap.to(".projects-slider h1", {
+//   transform: "translateX(-47%)",
+//   scrollTrigger: {
+//     // markers: true,
+//     trigger: ".projects-slider",
+//     scroller: ".container",
+//     scrub: 4,
+//     start: "top 0%",
+//     end: "top -100%",
+//     pin: true,
+//   },
+// });
